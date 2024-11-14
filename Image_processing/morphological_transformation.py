3216dceea389fb6d98ef17ff971d56f16a9de764
Dhaundiyal <108185538+Dhaundiyal@users.noreply.github.com>
2024-11-14 19:06:10 +0530

    Update morphological_transformation.py

--- Image_processing/morphological_transformation.py
+++ Image_processing/morphological_transformation.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('image1.jpg', 0)
+img = cv2.imread('image1.jpeg', 0)
 kernel = np.ones((5,5), np.uint8)
 
 opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
