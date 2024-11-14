53b3584f98674c5e17b6bf789aa26e24c44add70
Dhaundiyal <108185538+Dhaundiyal@users.noreply.github.com>
2024-11-14 19:03:36 +0530

    Update bgr2gray.py

--- Image_processing/bgr2gray.py
+++ Image_processing/bgr2gray.py
@@ -1,5 +1,5 @@
 import cv2
-image = cv2.imread('image2.jpg')
+image = cv2.imread('image2.jpeg')
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imwrite('image2.jpg', gray_image)
 cv2.imshow('Grayscale Image', gray_image)
