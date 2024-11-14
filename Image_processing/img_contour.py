062f9371227fed85eb300cd8ba21038c238ecb80
Dhaundiyal <108185538+Dhaundiyal@users.noreply.github.com>
2024-11-14 19:04:37 +0530

    Update img_contour.py

--- Image_processing/img_contour.py
+++ Image_processing/img_contour.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('image1.jpg', 0)
+img = cv2.imread('image1.jpeg', 0)
 _, threshold = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
